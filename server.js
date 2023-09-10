b5780f56544204ec7f9d64aeb41aebabc515fb63
Konan <kixeyems0@gmail.com>
2023-09-10 17:55:25 +0100

    added json parser, post request and test for it

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const port = 3000
 
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendStatus(200)
+})
+
+app.post('/', (req, res) => {
+  const { name, location } = req.body
+  res.status(200).send({
+    message: `your keys were ${name}, and ${location}` 
+  })
 })
 
 
